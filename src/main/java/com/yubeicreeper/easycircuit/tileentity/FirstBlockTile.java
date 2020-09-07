458eda430667de7b75a9431f0ddaee122255bd41
Dilirebug <54846078+Dilirebug@users.noreply.github.com>
2020-09-07 15:36:16 +0800

    修改FirstblockTile中的ItemStackHandler的初始化及调用方式

--- src/main/java/com/yubeicreeper/easycircuit/tileentity/FirstBlockTile.java
+++ src/main/java/com/yubeicreeper/easycircuit/tileentity/FirstBlockTile.java
@@ -19,43 +19,43 @@ import javax.annotation.Nullable;
  * @date 2020/9/7
  */
 public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
-    private ItemStackHandler itemStackHandler;
+    private LazyOptional<ItemStackHandler> handler = LazyOptional.of(this::createHandler);
 
     public FirstBlockTile() {
         super(ModBlocks.FIRSTBLOCK_TILE);
     }
 
-    private ItemStackHandler getHandler() {
-        if (itemStackHandler == null) {
-            itemStackHandler = new ItemStackHandler() {
-                @Override
-                public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-                    // 增加筛选功能
-                    return stack.getItem() == ModBlocks.FIRSTBLOCK.asItem();
-                }
-            };
-        }
-        return itemStackHandler;
+    private ItemStackHandler createHandler() {
+        return new ItemStackHandler() {
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                // 增加筛选功能
+                return stack.getItem() == ModBlocks.FIRSTBLOCK.asItem();
+            }
+        };
     }
 
     @Override
     public void tick() {
-        if (world.isRemote) {
-
-        }
+//        if (world.isRemote) {
+//
+//        }
     }
 
     @Override
     public void read(CompoundNBT tag) {
         CompoundNBT inv = tag.getCompound("inv");
-        getHandler().deserializeNBT(inv);
+        handler.ifPresent(h -> deserializeNBT(inv));
         super.read(tag);
     }
 
     @Override
     public CompoundNBT write(CompoundNBT tag) {
-        CompoundNBT compoundNBT = getHandler().serializeNBT();
-        tag.put("inv", compoundNBT);
+        handler.ifPresent(h -> {
+            CompoundNBT compoundNBT = h.serializeNBT();
+            tag.put("inv", compoundNBT);
+
+        });
         return super.write(tag);
     }
 
@@ -70,7 +70,7 @@ public class FirstBlockTile extends TileEntity implements ITickableTileEntity {
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
-            return LazyOptional.of(() -> (T)getHandler());
+            return handler.cast();
         }
         return super.getCapability(cap, side);
     }
